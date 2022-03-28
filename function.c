da7b4b728a5ab4002076f43f872ee076ad7a6310
bilal <muhammadbilalmnaeem@gmail.com>
2022-03-28 11:21:21 +0500

    function in C

--- function.c
+++ function.c
@@ -1,7 +1,10 @@
 #include <Stdio.h>
 
 // void sayHello(); // function prototype/decleration
-int main(int argc, char const *argv[])
+// int sum(int n1, int n2);
+char getChar();
+//* Main function
+int main()
 {
     // Function is building blocks of program which is used to make our program clean and make code reuseable
     // sayHello();
@@ -11,8 +14,28 @@ int main(int argc, char const *argv[])
     // sayHello();
     // sayHello();
 
+    // int val1 = sum(3, 5); // function call with arguments
+    // printf("%d", val1);
+    // int val2 = sum(56, 65); // function call with arguments
+    // printf("%d", val2);
+
+    char letter = getChar();
+    printf("%c", letter);
+
     return 0;
 }
+
+//* User Define Function
+char getChar()
+{
+    return 'c';
+}
+// int sum(int n1, int n2) // function with parameters
+// {
+//     int result = n1 + n2;
+//     return result;
+// }
+
 // void sayHello()
 // { // function defination
 // // void means return empty nothing to return
